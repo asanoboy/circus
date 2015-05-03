75b6447016b7f3b9d3c92bb0749b48e3a4f822f8
yosuke.asano <y.a.personal@gmail.com>
2015-05-03 12:24:38 +0900

    Added table.sql.

---
+++ src/table.sql
@@ -0,0 +1,7 @@
+create table page_ex (
+    page_id INT NOT NULL,
+    name varchar(255) NOT NULL,
+    infotype VARCHAR(255) NOT NULL,
+    infocontent TEXT NOT NULL,
+    primary key (page_id)
+);
