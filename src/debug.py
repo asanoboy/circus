dbe8a1d162f41e1a290c4b37b59df9b31ce760bc
yosuke.asano <anauser@localhost.localdomain>
2015-06-22 18:41:31 +0900

    Modified Lap.

--- src/debug.py
+++ src/debug.py
@@ -2,14 +2,20 @@ import time
 
 
 class Lap:
+    _level = 0
+
     def __init__(self, tag):
         self.start = None
         self.tag = tag
 
     def __enter__(self):
         self.start = time.time()
-        print('[%s]: start' % (self.tag,))
+        print('%s>> %s' % ('  ' * Lap._level, self.tag,))
+        Lap._level += 1
 
     def __exit__(self, exception_type, exception_value, traceback):
+        Lap._level -= 1
         interval = time.time() - self.start
-        print('[%s]: elapsed time = %d' % (self.tag, interval))
+        print(
+            '%s<< %s | elapsed time = %d' %
+            ('  ' * Lap._level, self.tag, interval))
