de4e34836565306d8d8cca91dc54245eabb6d61e
浅野洋介 <yosuke@yBook.local>
2015-07-24 00:03:35 +0900

    Updated.

--- src/executable/igraph_musician.py
+++ src/executable/igraph_musician.py
@@ -32,21 +32,33 @@ if True or __name__ == '__main__':
 
         g = Graph()
         logger.debug('page_num=%s' % (len(pid_to_page),))
-        for pid, page in pid_to_page.items():
-            if page.linknum >= 10:
-                g.add_vertex(str(pid), label=page.name)
-            else:
-                g.add_vertex(str(pid))
+        id_to_page = {}
+        pid_to_id = {}
+        names = ''
+        for index, (pid, page) in enumerate(pid_to_page.items()):
+            id_to_page[index] = page
+            pid_to_id[pid] = index
+            # g.add_vertex()
+            label = page.name
+            pos = label.find('(')
+            if pos >= 0:
+                label = label[:pos]
+            g.add_vertex(name=label)
+            names += ',' + label
 
         logger.debug('link_num=%s' % (len(links),))
-        g.add_edges([(str(r['id_from']), str(r['id_to'])) for r in links])
+        g.add_edges(
+            [[
+                pid_to_id[r['id_from']],
+                pid_to_id[r['id_to']]
+            ] for r in links])
         
-        method_to_comm = get_method_to_comm(g)
-        for method, c in method_to_comm.items():
-            save_cluster(c, method, box=(10000, 10000))
-            cl = as_clustering_if_not(c)
-            logger.debug(
-                'len = %s, mod = %s %s' % (
-                    len(cl.subgraphs()),
-                    cl.q,
-                    method))
+        # method_to_comm = get_method_to_comm(g)
+        # for method, c in method_to_comm.items():
+        #     save_cluster(c, method, box=(10000, 10000))
+        #     cl = as_clustering_if_not(c)
+        #     logger.debug(
+        #         'len = %s, mod = %s %s' % (
+        #             len(cl.subgraphs()),
+        #             cl.q,
+        #             method))
