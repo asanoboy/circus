13d556250f5644474a630ae4721d1937bd5c0173
yosuke.asano <anauser@localhost.localdomain>
2015-08-21 14:23:48 +0900

    Improved get_musician_graph_amzjp.py

--- src/executable/get_musician_graph_amzjp.py
+++ src/executable/get_musician_graph_amzjp.py
@@ -6,16 +6,36 @@ from urllib.parse import urlparse
 import os
 import time
 import random
+import hashlib
+import argparse
 
-data_dir = '/mnt/hdd500/amazon/jp/musician'
-init_url = 'http://www.amazon.co.jp/%E3%82%B1%E3%83%9F%E3%82%AB%E3%83%AB%E3%83%BB%E3%83%96%E3%83%A9%E3%82%B6%E3%83%BC%E3%82%BA/e/B000AQ22AU/'
-link_selector = '#entitySimsTable a'
+
+class AmazonHandler:
+    def __init__(self, lang):
+        if lang == 'ja':
+            self.data_dir = '/mnt/hdd500/amazon/jp/musician'
+        elif lang == 'us':
+            self.data_dir = '/mnt/hdd500/amazon/us/musician'
+        else:
+            raise 'Invalid lang: %s' % (lang,)
+
+        self.link_selector = '#entitySimsTable a'
+
+    def calc_data_path(self, page):
+        page_id = page.get_id()
+        dirname = hashlib.md5(page_id.encode()).hexdigest()[:2]
+        data_path = os.path.join(self.data_dir, dirname, page_id)
+        return data_path
+
+    def is_musician_dom(self, soup):
+        return len(soup.select('div.MusicCartBar')) > 0
 
 
 class Page:
     def __init__(self, url):
         self.url = url
         self.url_obj = urlparse(url)
+        self.content = None
 
     def get_id(self):
         path = self.url_obj.path
@@ -25,14 +45,9 @@ class Page:
         self.content = content
 
 
-def calc_data_path(page):
-    page_id = page.get_id()
-    data_path = os.path.join(data_dir, page_id[:4], page_id[4:])
-    return data_path
-
-
-def load_content(page, proxy, sleep):
-    data_path = calc_data_path(page)
+def load_content(page, amz, proxy, sleep):
+    logger = get_logger(__name__)
+    data_path = amz.calc_data_path(page)
     if os.path.exists(data_path):
         with open(data_path, 'r') as f:
             content = f.read()
@@ -40,7 +55,9 @@ def load_content(page, proxy, sleep):
             page.set_content(content)
             return True
 
-    html = get_html(page.url, proxy)
+    with logger.lap('get_html proxy=%s' % (proxy,)):
+        html = get_html(page.url, proxy=proxy, timeout=30)
+
     if html is None:
         return None
     page.set_content(html)
@@ -50,6 +67,8 @@ def load_content(page, proxy, sleep):
         os.makedirs(dir_path)
     with open(data_path, 'w') as f:
         f.write(html)
+
+    time.sleep(sleep)
     return True
 
 
@@ -67,7 +86,7 @@ def update_proxy():
         if candidates is None:
             time.sleep(5)
             trial -= 1
-        proxy_list = candidates[:50]
+        proxy_list = candidates[:20]
         proxy_updated_at = time.time()
         logger.debug('Succeed to update proxy', proxy_updated_at)
         return True
@@ -77,6 +96,21 @@ def update_proxy():
 if __name__ == '__main__':
     init_logger()
     logger = get_logger(__name__)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--lang', required=True)  # ja,us
+    args = parser.parse_args()
+    args = vars(args)
+    lang = args['lang']
+
+    amz = AmazonHandler(lang)
+
+    if lang == 'ja':
+        init_url = 'http://www.amazon.co.jp/%E3%82%B1%E3%83%9F%E3%82%AB%E3%83%AB%E3%83%BB%E3%83%96%E3%83%A9%E3%82%B6%E3%83%BC%E3%82%BA/e/B000AQ22AU/'
+    elif lang == 'us':
+        init_url = 'http://www.amazon.com/The-Chemical-Brothers/e/B000AQ22AU/'
+    else:
+        raise 'Invalid lang = %s' % (lang,)
     page_stack = [Page(init_url)]
     page_ids = [p.get_id() for p in page_stack]
 
@@ -86,13 +120,24 @@ if __name__ == '__main__':
                 raise 'Can\'t update proxy'
         logger.debug('stack_length = ', len(page_stack))
         page = page_stack.pop()
+        logger.debug('page_id=', page.get_id())
 
-        proxy = random.sample(proxy_list, 1)[0]
-        if not load_content(page, proxy, 10):
-            logger.debug('Load error: url = ', page.url)
+        for i in range(5):
+            proxy = random.sample(proxy_list, 1)[0]
+            if load_content(page, amz, proxy, 10):
+                break
+            logger.debug(
+                'Load error: url = ', page.url, 'proxy = ', proxy)
+
+        if page.content is None:
+            logger.debug(
+                'Can\'t load: url = ', page.url)
             continue
+
         sp = Soup(page.content, 'html.parser')
-        link_elems = sp.select(link_selector)
+        if not amz.is_musician_dom(sp):
+            continue
+        link_elems = sp.select(amz.link_selector)
         link_urls = [elem.get('href').strip() for elem in link_elems]
         for link_page in [Page(url) for url in link_urls]:
             page_id = link_page.get_id()
--- src/http_utils.py
+++ src/http_utils.py
@@ -4,7 +4,21 @@ from urllib.parse import urlparse, urlencode
 from bs4 import BeautifulSoup as Soup
 
 
-def get(url_str, proxy=None):
+class ProxyHost:
+    def __init__(self, host_and_port, region):
+        host, port = host_and_port.split(':')
+        self.host = host
+        self.port = port
+        self.region = region
+
+    def __str__(self):
+        if self.region:
+            return '%s:%s(%s)' % (self.host, self.port, self.region)
+        else:
+            return '%s:%s' % (self.host, self.port)
+
+
+def get(url_str, proxy=None, timeout=None):
     '''
     @param proxy like '1.1.1.1:8080'
     '''
@@ -16,11 +30,11 @@ def get(url_str, proxy=None):
         port = url.port
 
     if proxy:
-        p_host, p_port = proxy.split(':')
-        conn = http.client.HTTPConnection(p_host, p_port)
+        conn = http.client.HTTPConnection(
+            proxy.host, proxy.port, timeout=timeout)
         conn.request('GET', url_str)
     else:
-        conn = http.client.HTTPConnection(url.hostname, port)
+        conn = http.client.HTTPConnection(url.hostname, port, timeout=timeout)
         conn.request('GET', url.path)
 
     res = conn.getresponse()
@@ -30,16 +44,24 @@ def get(url_str, proxy=None):
 content_type_pattern = re.compile('charset=(.+)$', re.IGNORECASE)
 
 
-def get_html(url_str, proxy=None, encode=None):
-    res = get(url_str, proxy=proxy)
+def get_html(url_str, proxy=None, encode=None, timeout=None):
+    try:
+        res = get(url_str, proxy=proxy, timeout=timeout)
+    except:
+        return None
+
     if res.code == 200:
+        try:
+            content = res.readall()
+        except:
+            return None
         if encode:
-            return res.readall().decode(encode)
+            return content.decode(encode)
         content_type = res.getheader('content-type')
         mat = content_type_pattern.search(content_type)
         if mat is None:
             raise 'Invalid content_type: %s' % (content_type,)
-        return res.readall().decode(mat.group(1))
+        return content.decode(mat.group(1))
 
     return None
 
@@ -63,4 +85,8 @@ def get_proxy_list():
     if not html:
         return None
     sp = Soup(html, 'html.parser')
-    return [el.get_text() for el in sp.select('a.A')]
+    return [
+        ProxyHost(
+            el.get_text(),
+            el.attrs['title'] if el.has_attr('title') else None)
+        for el in sp.select('a.A')]
